41ac056b6c0df1a92d220812d921155e4c63020c
Elina Shramko <elina.vl.shramko@gmail.com>
2024-06-25 15:48:40 +0300

    Add initial models for Team and Person

--- test_task_shramko/teams/models.py
+++ test_task_shramko/teams/models.py
@@ -1,1 +1,13 @@
-# Create your models here.
+from django.db import models
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+
+    teams = models.ManyToManyField(Team, related_name="people")
